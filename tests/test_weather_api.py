5db3f942cd319abc5a726a55e4bece4cc035e34d
XetPy1030 <xetpy1030@mail.ru>
2024-02-08 17:54:26 +0300

    Add test city not found

--- tests/test_weather_api.py
+++ tests/test_weather_api.py
@@ -16,3 +16,10 @@ class SimpleTest(TestCase):
 
         assert response.status_code == 200
         assert response.data is not None
+
+    def test_city_not_found(self):
+        request = self.factory.get("/weather", {"city": "oawdowadij"})
+        response = WeatherView.as_view()(request)
+
+        assert response.status_code == 500
+        assert response.data['detail'] == 'City not found'
